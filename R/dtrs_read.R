63e4dac9fe0c59f5b20bfe2223d822add2749d24
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-09-04 17:05:17 +0000

    ammended dtrs_read

--- R/dtrs_read.R
+++ R/dtrs_read.R
@@ -5,16 +5,18 @@
 #' dataframes, "HH", "HL" or "CA". This format is what is returned when using
 #' the dtrs_download function.
 #'
+#' @param type The table type, one of "HH", "HL" or "CA"
 #' @param surveys Character vector containing survey names, e.g. c("NS-IBTS",
 #' ROCKALL").
-#' @param type The table type, one of "HH", "HL" or "CA"
+#' @param years What years?
+#' @param quarters What quarters?
 #' @param path The directory path of were the data is stored.
 #'
 #' @return A tibble
 #'
 #' @export
 #'
-dtrs_read <- function(surveys, type = "HH", path = "data-raw/datras") {
+dtrs_read <- function(type = "HH", surveys, years, quarters, path = "data-raw/datras") {
 
   Type <- type
 
@@ -25,7 +27,15 @@ dtrs_read <- function(surveys, type = "HH", path = "data-raw/datras") {
     tidyr::separate(survey, c("survey", "year", "quarter", "type"), sep = "_", convert = TRUE) %>%
     tidyr::drop_na()
 
-  if(!missing(surveys)) files <- files %>% dplyr::filter(survey %in% surveys)
+  if(!missing(surveys)) {
+    files <- files %>% dplyr::filter(survey %in% surveys)
+  }
+  if(!missing(years)) {
+    files <- files %>% dplyr::filter(year %in% years)
+  }
+  if(!missing(quarters)) {
+    files <- files %>% dplyr::filter(quarter %in% quarters)
+  }
 
   files <-
     files %>%
