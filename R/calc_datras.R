60ad9427306a9b0383e3d78e669609099cb272af
Martin Pastoors <martin.pastoors@gmail.com>
2023-02-08 08:05:06 +0100

    cpue calculator

---
+++ R/calc_datras.R
@@ -0,0 +1,77 @@
+#' Calculate with DATRAS data
+#'
+#' @param hh A tidy haul object obtained from tidyices::dr_getdata
+#'
+#' @return A tibble of the same dimention as the input
+#' @export
+#'
+dr_calccpue <- function(d, hh) {
+
+  colnames(d) <- tolower(colnames(d))
+
+  d <- d |> tibble::as_tibble()
+
+  if(unique(d$recordtype) == "HL") d <- d |> dr_calccpue_hl(hh)
+  if(unique(d$recordtype) == "CA") d <- d |> dr_calccpue_ca(hh)
+
+  return(d)
+
+}
+
+
+
+#' dr_calccpue_hl
+#'
+#' @param d DATRAS raw length dataframe, as obtained via the function
+#' icesDatras::getDATRAS
+#'
+#' @return A tibble
+#'
+#'
+dr_calccpue_hl <- function(d, hh) {
+
+  d <-
+    d |>
+
+    #left join hh
+    dplyr::left_join(hh %>% dplyr::select(id,
+                                          datatype, hauldur),
+                     by = "id") |>
+
+    # catch per hour
+    dplyr::mutate(nperhour = dplyr::case_when(
+         datatype == "R"  ~ hlnoatlngt * 60 / hauldur,
+         datatype == "C"  ~ hlnoatlngt,
+         TRUE             ~ as.numeric(NA)))
+
+  return(d)
+
+}
+
+#' dr_calccpue_hl
+#'
+#' @param d DATRAS raw length dataframe, as obtained via the function
+#' icesDatras::getDATRAS
+#'
+#' @return A tibble
+#'
+#'
+dr_calccpue_ca <- function(d, hh) {
+
+  d <-
+    d |>
+
+    #left join hh
+    dplyr::left_join(hh %>% dplyr::select(id,
+                                          datatype, hauldur),
+                     by = "id") |>
+
+    # catch per hour
+    dplyr::mutate(nperhour = dplyr::case_when(
+      datatype == "R"  ~ canoatlngt * 60 / hauldur,
+      datatype == "C"  ~ canoatlngt,
+      TRUE             ~ as.numeric(NA)))
+
+    return(d)
+
+}
