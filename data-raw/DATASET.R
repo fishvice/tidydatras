f765be3276b67051d044976b137ba3ce83c848a1
Martin Pastoors <martin.pastoors@gmail.com>
2023-02-09 14:40:04 +0100

    added reco to dataset.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -102,3 +102,18 @@ ca |>
 
 aphia_latin <- dws
 usethis::use_data(aphia_latin)
+
+## Vessel codes https://vocab.ices.dk/?ref=315 -------------------------------------------------
+
+reco <-
+  read.csv("data-raw/RECO_Export_09-26-2023-06-26-45.csv", colClasses = "character")  %>%
+  dplyr::rename_all(tolower) %>%
+  dplyr::select(
+    code,
+    vessel = description) %>%
+  dplyr::mutate(
+    vessel = ifelse(code=="29JR", "JOSE RIOJA", vessel)
+  )
+save(reco, file="data/reco.rda")
+usethis::use_data(reco, overwrite=TRUE)
+
